ad73d9a83627414b60b03c7a4c591e7e34b6fd79
unknown <banupama225@gmail.com>
2024-09-28 17:30:40 +0530

    second commit

--- features/admin.py
+++ features/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from features.models import Members,Trainsmodel,stationmodel,schedulemodel,Reservationmodel
+from features.models import Members,Trainsmodel,stationmodel,schedulemodel,Reservationmodel,paymentmodel
 
 # Register your models here.
 admin.site.register(Members)
@@ -7,3 +7,5 @@ admin.site.register(Trainsmodel)
 admin.site.register(stationmodel)
 admin.site.register(schedulemodel)
 admin.site.register(Reservationmodel)
+
+admin.site.register(paymentmodel)
